54a1d01dbdf36d2b2e99737c7996059363aa9849
Sunil Sankar <sunil@sunil.cc>
2022-07-01 06:13:19 +0200

    Added couple of parameters to avoid the out of memory issue
    OpenJDK 64-Bit Server VM warning: INFO: os::commit_memory(0x0000000700000000, 4294967296, 0) failed; error='Not enough space' (errno=12) shutdown.sh

--- setenv.sh
+++ setenv.sh
@@ -1,2 +1,2 @@
-JAVA_OPTS="-Xms4096m -Xmx4096m"
-JAVA_OPTS="$JAVA_OPTS -DNodeType=Stream,BackgroundProcessing,WebUser,Search"
+JAVA_OPTS="-Xms4096m -Xmx4096m -XX:+CMSClassUnloadingEnabled -XX:TargetSurvivorRatio=90 -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+DisableExplicitGC -XX:+UseCMSInitiatingOccupancyOnly  -XX:ReservedCodeCacheSize=756m -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=80 -XX:NewSize=256"
+JAVA_OPTS="$JAVA_OPTS -DNodeType=Stream,BackgroundProcessing,WebUser,Search
