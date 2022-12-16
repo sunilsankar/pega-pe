4339facdf1edcea2e0bbdb21197b918b93ed4a9f
Sunil Sankar <sunil@sunil.cc>
2022-12-16 02:09:53 +0100

    Issue command not found

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ deb () {
     arch
     sudo apt update -y
     sudo apt install postgresql-11 -y
-    sudo apt install curl ca-certificates gnupg unzip wget -y
+    sudo apt install curl ca-certificates gnupg unzip wget lsb-release -y
     curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg >/dev/null
     sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
     sudo apt update -y
